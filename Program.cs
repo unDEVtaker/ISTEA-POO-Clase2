8f1c83e958b021ee0c510956221407f8f7890feb
Jona Silva <elundevtaker@gmail.com>
2023-06-28 00:00:52 -0300

    cierre clase2 - temas vistos:Clase,metodos, campos, constructores y sobrecargas.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+//Temas vistos:clase, metodos, campos y constructores.
+
 Alumno alumno1 = new Alumno(); //esquema clasico para crear un objeto de la clase.
 alumno1.Apellido="Alonso";
 alumno1.Nombre="Laura";
@@ -14,15 +16,32 @@ Console.WriteLine(alumno1.Clave);
 
 alumno1.DNI=222222;
 Console.WriteLine(alumno1.DNI);
+
+var alumno3=new Alumno();
+Console.WriteLine(alumno3.Apellido);
+
+var alumno4=new Alumno();
+Console.WriteLine(alumno4.Nombre);
+
+var alumno5= new Alumno(444444);
+Console.WriteLine(alumno5.DNI); 
 class Alumno{
-        private string nombre;
+    public Alumno(){ //constructor
+        Apellido= "Roso";
+    }
+    public Alumno (String nombreInicial){
+        Nombre=nombreInicial;
+    }
+    public Alumno (int documentoOriginal){
+        documento=documentoOriginal;
+    }
         private string claveGuardada;
         private int documento;  
         public string Clave{//intermediario 
             get{return claveGuardada;}
             set{claveGuardada=value;} //lo busca en el program
         }
-        public string Nombre { get; set; }
+        public string Nombre { get; set; } //prop autoimplementada
         public string Apellido { get; set; }
         public int DNI {
             get{return documento;}
